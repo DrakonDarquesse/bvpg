3ebc623980445d1b521e67ee9889e27ce019f29b
Ming Liang Tay <mingliang@bim.local>
2024-06-01 14:36:06 +0800

    refactor: contextmixin, slidetemplate mixin, separate logic of copy, render data and create new slide

--- api/presentation_builder.py
+++ api/presentation_builder.py
@@ -14,6 +14,8 @@ env = Environment(
     variable_end_string='@'
 )
 
+TITLE_SLIDE_LAYOUT_INDEX = 5
+
 
 class BibleBookVerbose(str, Enum):
     genesis = "创世记"
@@ -34,19 +36,237 @@ def format_overline(passage: Passage):
         return f'{book} {passage.start_verse.chapter}:{passage.start_verse.verse}-{passage.end_verse.chapter}:{passage.end_verse.verse}'
 
 
-class PresentationBuilder:
+class ContextBuilder:
+    pass
+
+
+def duplicate_slide(source_slide, target_slide):
+    """
+    copy background (if its solid color) and all texts along with stylings.
+    """
+
+    # set background is its solid fill
+    try:
+
+        if not source_slide.follow_master_background:
+            if source_slide.background.fill.type == MSO_FILL.SOLID:
+                target_slide.background.fill.solid()
+                target_slide.background.fill.fore_color.rgb = source_slide.background.fill.fore_color.rgb
+    except:
+        pass
+
+    title = source_slide.shapes.title
+
+    target_title = target_slide.shapes.title
+
+    target_title.left = title.left
+    target_title.top = title.top
+    target_title.width = title.width
+    target_title.height = title.height
+
+    target_title.text_frame.word_wrap = True
+    target_title.text_frame.vertical_anchor = title.text_frame.vertical_anchor
+
+    for paragraph in title.text_frame.paragraphs:
+        # when iterating paragraph, update the last paragraph in new_shape.text_frame
+        new_paragraph = target_title.text_frame.paragraphs[len(
+            target_title.text_frame.paragraphs)-1]
+        new_paragraph.alignment = paragraph.alignment
+        # print(new_paragraph.alignment)
+
+        for run in paragraph.runs:
+            new_run = new_paragraph.add_run()
+            new_run.text = run.text
+            new_run.font.name = run.font.name
+            new_run.font.size = run.font.size
+            new_run.font.bold = run.font.bold
+            new_run.font.italic = run.font.italic
+            new_run.font.underline = run.font.underline
+            new_run.font._element.set(
+                'baseline', run.font._element.get('baseline', '0'))
+            if run.font.color.rgb is not None:
+                new_run.font.color.rgb = run.font.color.rgb
+                print(new_run.font.color.rgb)
+
+        if len(target_title.text_frame.paragraphs) != len(title.text_frame.paragraphs):
+            target_title.text_frame.add_paragraph()
+
+    for shape in source_slide.shapes:
+        if not shape.has_text_frame:
+            continue
+
+        if shape == title:
+            continue
+
+        new_shape = target_slide.shapes.add_textbox(shape.left, shape.top, shape.width,
+                                                    shape.height)  # add a new textbox to the slide
+        new_shape.text_frame.word_wrap = True
+        new_shape.text_frame.vertical_anchor = shape.text_frame.vertical_anchor
+        for paragraph in shape.text_frame.paragraphs:
+            # when iterating paragraph, update the last paragraph in new_shape.text_frame
+            new_paragraph = new_shape.text_frame.paragraphs[len(
+                new_shape.text_frame.paragraphs)-1]
+            new_paragraph.alignment = paragraph.alignment
+
+            for run in paragraph.runs:
+                new_run = new_paragraph.add_run()
+                new_run.text = run.text
+                new_run.font.name = run.font.name
+                new_run.font.size = run.font.size
+                new_run.font.bold = run.font.bold
+                new_run.font.italic = run.font.italic
+                new_run.font.underline = run.font.underline
+                new_run.font._element.set(
+                    'baseline', run.font._element.get('baseline', '0'))
+                if run.font.color.rgb is not None:
+                    new_run.font.color.rgb = run.font.color.rgb
+
+            if len(new_shape.text_frame.paragraphs) != len(shape.text_frame.paragraphs):
+                new_shape.text_frame.add_paragraph()
+
+
+def render_slide_data(slide, context):
+
+    # slide.shapes.title.text = context['data'].get(
+    #     'title', slide.shapes.title.text)
+
+    print(slide.shapes.title.text_frame.paragraphs[0].alignment)
+    slide.shapes.title.text_frame.paragraphs[0].runs[0].text = context['data'].get(
+        'title', slide.shapes.title.text)
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+
+        for paragraph in shape.text_frame.paragraphs:
+
+            for run in paragraph.runs:
+                run.text = env.from_string(
+                    run.text).render(context['data'])
+
+
+class PresentationTemplateMixin:
     template = None
+
+    def get_template(self):
+        return self.template
+
+    def get_slides(self):
+        print("getting slides")
+        # only returns slide with title
+        return list(filter(lambda slide: bool(slide.shapes.title), self.get_template().slides))
+
+    def get_slide(self, title):
+        return next(
+            (slide for slide in self.get_slides() if slide.shapes.title.text == title))
+
+
+class PresentationBuilder:
     base_file = None
     save_file_name: str | None = None
 
+    def get_base_file(self):
+        return self.base_file
+
     def save_file(self):
+        self.base_file.save(self.save_file_name)
+
+    def new_slide(self):
+        base_file = self.get_base_file()
+        return base_file.slides.add_slide(
+            base_file.slide_layouts[TITLE_SLIDE_LAYOUT_INDEX])
+
+    def build(self):
         raise NotImplementedError("Should have implemented this")
 
-    def build_slide(self):
-        raise NotImplementedError("Should have implemented this")
+
+class ContextMixin:
 
     def get_context(self):
         raise NotImplementedError("Should have implemented this")
+
+
+class PassageContext(ContextMixin):
+    passages: list[Passage] = []
+
+    def get_context(self):
+        return [self.cover(), *self.verse()]
+
+    def get_passages(self):
+        return self.passages
+
+    def cover(self):
+        passages = self.get_passages()
+        if len(passages) > 1:
+            pass
+        else:
+            passage = passages[0]
+            context = {
+                'title': 'one_passage_cover',
+                'data': {
+                    'title': '读经',
+                    'chi_book': BibleBookVerbose[passage.book.name].value,
+                    'eng_book': passage.book.name,
+                }
+            }
+            if passage.is_same_chapter():
+                context['data'].update({
+                    'verse': f'{passage.start_verse.chapter}: {passage.start_verse.verse}-{passage.end_verse.verse}'
+                })
+            else:
+                context['data'].update({
+                    'verse': f'{passage.start_verse.chapter}: {passage.start_verse.verse}-{passage.end_verse.chapter}: {passage.end_verse.verse}'
+                })
+            return context
+
+    def verse(self):
+        slide_contexts = []
+        passages = self.get_passages()
+        for passage in passages:
+            english_passage_api = KjvPassageApi()
+            chinese_passage_api = CuvsPassageApi()
+
+            english_verses = english_passage_api.retrieve_passage(
+                passage)
+            chinese_verses = chinese_passage_api.retrieve_passage(
+                passage)
+            overline = format_overline(passage=passage)
+
+            for i, verse in enumerate(english_verses):
+                slide_contexts.append({
+                    'title': 'verse',
+                    'data': {
+                        'title': overline,
+                        'verse_num': verse[0],
+                        'chi_verse': chinese_verses[i][1],
+                        'eng_verse': verse[1]}
+                })
+        return slide_contexts
+
+
+class PassagePresentationBuilder(PresentationBuilder, PresentationTemplateMixin, PassageContext):
+    template = Presentation("passage_template.pptx")
+
+    def __init__(self, save_file_name: str = 'slide.pptx', base_name: str | None = None, passages: list[Passage] = []) -> None:
+        self.save_file_name = save_file_name
+        self.base_file = Presentation(base_name)
+        self.passages = passages
+
+    def build(self):
+        """
+        get slide templates and use them to add new slide to base file
+        """
+
+        print("start building slides")
+        contexts = self.get_context()
+        for context in contexts:
+            self.build_slide(context)
+
+    def build_slide(self, context):
+        template_slide = self.get_slide(context["title"])
+        new_slide = self.new_slide()
+
+        duplicate_slide(template_slide, new_slide)
+        render_slide_data(new_slide, context)
 
 
 class BibleVersePresentationBuilder(PresentationBuilder):
@@ -336,3 +556,11 @@ class ResponsivePresentationBuilder(PresentationBuilder):
             'eng_close': english_verses[last_index][1],
             'responsive_end': ''
         }
+
+
+passages = [Passage(book=BibleBook.genesis, start_verse=Verse(
+    chapter=1, verse=1), end_verse=Verse(chapter=1, verse=2))]
+slide = PassagePresentationBuilder(
+    base_name="base_wide.pptx", passages=passages)
+slide.build()
+slide.save_file()
