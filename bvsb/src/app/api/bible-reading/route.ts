99ab8a1226dffca0001d4e89e09861962cf89a44
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-06-26 11:52:13 +0800

    refactor(bible-reading): update ui, route

---
+++ bvsb/src/app/api/bible-reading/route.ts
@@ -0,0 +1,11 @@
+export async function POST(request: Request) {
+  const json = await request.json();
+  console.log(json);
+  return await fetch("http://127.0.0.1:8000/bvpg/slides/bible-reading", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(json),
+  });
+}
