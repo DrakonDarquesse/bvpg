e7bb97e65660f94f7a4e0427c050d4a559ee38e0
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-07-01 17:43:37 +0800

    refactor(api/bible-reading): use PassageList

--- api/presentation_builder.py
+++ api/presentation_builder.py
@@ -1,8 +1,8 @@
 from pptx import Presentation
 from data.bible import BibleBookVerbose
 from base import ContextMixin, PresentationBuilder, PresentationTemplateMixin, duplicate_slide, render_slide_data
-from bible_passage_api import CuvsPassageApi, KjvPassageApi
-from models import BibleBook, Passage, Verse
+from bible_passage_api import PassageList
+from models import Passage
 from environment import env
 
 
@@ -19,8 +19,16 @@ def format_overline(passage: Passage):
 
 class PassageContext(ContextMixin):
     passages: list[Passage] = []
+    combined_verses = []
 
     def get_context(self):
+        passages = self.get_passages()
+        for passage in passages:
+            self.combined_verses.append(
+                {
+                    'combined_verses': PassageList(passage=passage).passages,
+                    'passage': passage
+                })
         return [self.cover(), *self.verse()]
 
     def get_passages(self):
@@ -52,21 +60,9 @@ class PassageContext(ContextMixin):
 
     def verse(self):
         slide_contexts = []
-        passages = self.get_passages()
-        for passage in passages:
-            english_passage_api = KjvPassageApi()
-            chinese_passage_api = CuvsPassageApi()
-
-            english_verses = english_passage_api.retrieve_passage(
-                passage)
-            chinese_verses = chinese_passage_api.retrieve_passage(
-                passage)
-            overline = format_overline(passage=passage)
-
-            combined_verses = combine_chi_eng_verses(
-                chinese_verses, english_verses)
-
-            for verse in combined_verses:
+        for combined_verses in self.combined_verses:
+            overline = format_overline(passage=combined_verses['passage'])
+            for verse in combined_verses['combined_verses']:
                 slide_contexts.append({
                     'title': 'verse',
                     'data': {
@@ -105,30 +101,3 @@ class PassagePresentationBuilder(PresentationBuilder, PresentationTemplateMixin,
 
         duplicate_slide(template_slide, new_slide)
         render_slide_data(new_slide, context, env)
-
-
-# passages = [Passage(book=BibleBook.ruth, start_verse=Verse(
-#     chapter=1, verse=1), end_verse=Verse(chapter=1, verse=17))]
-# slide = PassagePresentationBuilder(
-#     base_name="template/base_wide.pptx", passages=passages)
-# slide.build()
-# slide.save_file()
-
-
-def combine_chi_eng_verses(chi_verses: list, eng_verses: list):
-    verses = []
-    while len(eng_verses) > 0:
-        if int(eng_verses[0][0]) == chi_verses[0][0]:
-            eng_verse = eng_verses.pop(0)
-            chi_verse = chi_verses.pop(0)
-            verses.append({
-                'num': eng_verse[0],
-                'eng': [eng_verse[1]],
-                'chi': [chi_verse[1]],
-            })
-        # if one verse its number is smaller, append it to the last in the verses
-        elif int(eng_verses[0][0]) < chi_verses[0][0]:
-            verses[-1]['eng'].append(eng_verses.pop(0)[1])
-        elif int(eng_verses[0][0]) > chi_verses[0][0]:
-            verses[-1]['chi'].append(chi_verses.pop(0)[1])
-    return verses
