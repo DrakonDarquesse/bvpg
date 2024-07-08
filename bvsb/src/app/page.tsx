2776b1027117671980213fc81afbb58700c8ae87
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-07-08 15:57:52 +0800

    feat(component/footer): create and use footer component

--- bvsb/src/app/page.tsx
+++ bvsb/src/app/page.tsx
@@ -10,11 +10,9 @@ import PassageForm, { Passage } from "@/web/components/passage-form";
 import Typography from "@mui/material/Typography";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
-import Link from "@mui/material/Link";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import MailIcon from "@mui/icons-material/Mail";
 import useTheme from "@mui/material/styles/useTheme";
 import Header from "@/web/components/header";
+import Footer from "@/web/components/footer";
 
 const BibleReading = () => {
   const theme = useTheme();
@@ -217,32 +215,7 @@ const BibleReading = () => {
           </Button>
         </Box>
       </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-start",
-          gap: 1,
-          alignItems: "center",
-          width: "100%",
-          "> a": {
-            display: "flex",
-            justifyContent: "center",
-            p: 1,
-            borderRadius: 2,
-            fontSize: theme.typography.h5,
-            ":hover": {
-              bgcolor: "primary.light",
-            },
-          },
-        }}
-      >
-        <Link href="mailto:mingliangzheng17@gmail.com">
-          <MailIcon fontSize="inherit"></MailIcon>
-        </Link>
-        <Link href="https://github.com/DrakonDarquesse/bvpg">
-          <GitHubIcon fontSize="inherit"></GitHubIcon>
-        </Link>
-      </Box>
+      <Footer></Footer>
     </Box>
   );
 };
---
+++ bvsb/src/web/components/footer.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MailIcon from "@mui/icons-material/Mail";
+import useTheme from "@mui/material/styles/useTheme";
+
+const Footer = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "flex-start",
+        gap: 1,
+        alignItems: "center",
+        width: "100%",
+        "> a": {
+          display: "flex",
+          justifyContent: "center",
+          p: 1,
+          borderRadius: 2,
+          fontSize: theme.typography.h5,
+          ":hover": {
+            bgcolor: "primary.light",
+          },
+        },
+      }}
+    >
+      <Link href="mailto:mingliangzheng17@gmail.com">
+        <MailIcon fontSize="inherit"></MailIcon>
+      </Link>
+      <Link href="https://github.com/DrakonDarquesse/bvpg">
+        <GitHubIcon fontSize="inherit"></GitHubIcon>
+      </Link>
+    </Box>
+  );
+};
+
+export default Footer;
