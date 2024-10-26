7c004fbd5bb95269ede16212bf51d8f1e8ee3ae5
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-10-26 13:43:43 +0800

    refactor(main/api): refactor based on new PresentationBuilder, save Presentation to buffer and return it

--- api/main.py
+++ api/main.py
@@ -1,30 +1,39 @@
-from typing import Annotated, Any
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
+from pptx import Presentation
+from io import BytesIO
 
-from responsive_presentation_builder import ResponsivePresentationBuilder
-from presentation_builder import PassagePresentationBuilder
-from models import Passage
+from bible_passage_api import CuvsPassageApi
+from data.bible import BibleBook
+from base import BlankPresentation, PresentationBuilder, PresentationTemplate
+from responsive_context import ResponsiveContext
+from passage_context import PassageContext
+from models import Passage, Verse
 
 app = FastAPI()
 
 
-@app.post("/bvpg/slides/bible-reading", response_class=FileResponse)
-async def build_slide_bible_reading(passages: list[Passage]):
-    slide = PassagePresentationBuilder(
-        base_name="template/base_wide.pptx", passages=passages)
+@app.post("/bvpg/slides/bible-reading", response_class=Response)
+def build_slide_bible_reading(passages: list[Passage]):
+    output = BytesIO()
+    slide = PresentationBuilder(
+        presentation=BlankPresentation(), template=PresentationTemplate(
+            Presentation("template/passage_template.pptx")), context=PassageContext(passages))
+
     slide.build()
-    slide.save_file()
-    return "slide.pptx"
+    slide.presentation.save_file(output)
+    return Response(output.getvalue(), media_type='application/pptx')
 
 
-@app.post("/bvpg/slides/responsive-reading", response_class=FileResponse)
+@app.post("/bvpg/slides/responsive-reading", response_class=Response)
 async def build_slide_responsive_reading(passages: list[Passage]):
-    slide = ResponsivePresentationBuilder(
-        base_name="template/base_wide.pptx", passages=passages)
+    output = BytesIO()
+    slide = PresentationBuilder(
+        presentation=BlankPresentation(), template=PresentationTemplate(
+            Presentation("template/responsive_reading.pptx")), context=ResponsiveContext(passages))
     slide.build()
-    slide.save_file()
-    return "slide.pptx"
+    slide.presentation.save_file(output)
+    return Response(output.getvalue(), media_type='application/pptx')
 
 
 @app.post("/bvpg/slides/passage", response_model=list[Passage])
