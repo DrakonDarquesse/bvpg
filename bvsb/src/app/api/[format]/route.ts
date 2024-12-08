bd31754adec7b4ea7d262dce1d53ca490b58eb43
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-12-09 00:07:51 +0800

    testing deployment with console log

--- bvsb/src/app/api/[format]/route.ts
+++ bvsb/src/app/api/[format]/route.ts
@@ -2,6 +2,7 @@ export async function POST(
   request: Request,
   { params }: { params: Promise<{ format: string }> }
 ) {
+  console.log("something something");
   const format = (await params).format;
   const json = await request.json();
   return await fetch(`${process.env.PASSAGE_API}/bvpg/slides/${format}`, {
