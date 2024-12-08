8d4ba878028eae3af02c595d651f6dc78eb57e09
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-12-08 23:31:01 +0800

    fix(next/route): use env var for passage api, and use dynamic route

---
+++ bvsb/src/app/api/[format]/route.ts
@@ -0,0 +1,14 @@
+export async function POST(
+  request: Request,
+  { params }: { params: Promise<{ format: string }> }
+) {
+  const format = (await params).format;
+  const json = await request.json();
+  return await fetch(`${process.env.PASSAGE_API}/bvpg/slides/${format}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(json),
+  });
+}
