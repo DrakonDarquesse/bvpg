f2016ca4736db00fe89937cbe732b33effcb8306
Ming Liang Tay <mingliangzheng17@gmail.com>
2025-04-11 17:19:30 +0800

    chore: remove console log

--- bvsb/src/app/api/[format]/route.ts
+++ bvsb/src/app/api/[format]/route.ts
@@ -2,7 +2,6 @@ export async function POST(
   request: Request,
   { params }: { params: Promise<{ format: string }> }
 ) {
-  console.log("something something");
   const format = (await params).format;
   const json = await request.json();
   return await fetch(`${process.env.PASSAGE_API}/bvpg/slides/${format}`, {
